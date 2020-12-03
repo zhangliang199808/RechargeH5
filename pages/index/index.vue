d876e5c5c24176690c84d54519ad5793e34c52b2
wxd1610 <47845582+wxd1610@users.noreply.github.com>
2020-12-03 23:32:43 +0800

    shishi

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,69 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<!-- <container style="width:100%"> -->
+		<ls-swiper :list="base_lsit" imgKey="imgUrl" :loop="true" :dots='true' :autoplay='true' style="width:100%" @clickItem="clickItem()" />
+		<!-- </container> -->
+		<!-- <textscroll :list="list"/> -->
+		<uni-notice-bar showIcon="true" scrollable="true" text="[多行] 这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏"></uni-notice-bar>
+
+		<!-- <image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
+		</view> -->
+		<view class="toplist">
+			<view class="list" v-for="(item,index) in topList" :key="index">
+				<image src="" mode="" style="display:block;width:90rpx;height:90rpx;background:#ccc;margin:0 auto"></image>
+				<view class="listtext">
+					{{item.name}}
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import uniNoticeBar from '@/components/uni-notice-bar/uni-notice-bar.vue'
+	import lsSwiper from '../../components/ls-swiper/index.vue'
 	export default {
+		components: {
+			lsSwiper,
+			uniNoticeBar,
+		},
 		data() {
 			return {
-				title: 'Hello'
+				list: ['1分钟前，无夏购买了会员', '2分钟前，无夏购买了会员', '3分钟前，无夏购买了会员'],
+				base_lsit: [{
+						// name: '1',
+						imgUrl: 'https://picb.zhimg.com/80/v2-3f5c7bdcc228d3798a044b78023459d3_720w.jpg?source=1940ef5c',
+					},
+					{
+						// name: '2',
+						imgUrl: 'https://pic3.zhimg.com/80/v2-1deed0eca463f87f36d6e88363ae2693_720w.jpg?source=1940ef5c',
+					},
+					{
+						// name: '3',
+						imgUrl: 'https://picb.zhimg.com/80/v2-520f230f8cb8a0943b8582dcd77ec6ba_720w.jpg',
+					},
+					{
+						// name: '4',
+						imgUrl: 'https://pic1.zhimg.com/80/v2-33e85bdfafbb4aa52ce6f70f319dbb4f_720w.jpg',
+					},
+				], //轮播图
+				topList: [{
+					name:'供热缴费',
+				},
+				{
+					name:"水电燃气"
+				},
+		{name:"投诉报修"},
+		{name:"卡号管理"},
+		{name:"电子发票"},
+		{name:"快票儿"},
+		{name:"网点地图"},
+		{name:"业务办理"},
+		// {name:"电子发票"},
+		// {name:"电子发票"},
+				],
 			}
 		},
 		onLoad() {
@@ -48,5 +100,18 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	.toplist{
+		display:flex;
+		flex-wrap:wrap
+	}
+	.list{
+		width:25%;
+		margin:20rpx 0
+	}
+	.listtext{
+		text-align:center;
+		font-size:20rpx;
+		margin-top:10rpx
 	}
 </style>
